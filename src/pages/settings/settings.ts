2fbf926c78e2f9c168e4c78018cb1e10555d0f55
andreakpresley <andreapresley5@gmail.com>
2017-08-06 22:48:04 -0500

    initial settings stuff
    -added code to change team names

--- src/pages/settings/settings.ts
+++ src/pages/settings/settings.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
+import { GlobalVarsService } from '../../services/globalVars.service';
 
 @Component({
   selector: 'page-settings',
@@ -7,8 +8,21 @@ import { NavController } from 'ionic-angular';
 })
 export class SettingsPage {
 
-  constructor(public navCtrl: NavController) {
+  private team1Name:string;
+  private team2Name:string;
+  private timerLength:number;
 
+  constructor(public navCtrl: NavController, private globalVarsService: GlobalVarsService) {
+    this.team1Name = globalVarsService.getTeam1Text();
+    this.team2Name = globalVarsService.getTeam2Text();
+  }
+
+  private team1NameChanged() {
+    this.globalVarsService.setTeam1Text(this.team1Name);
+  }
+
+  private team2NameChanged() {
+    this.globalVarsService.setTeam2Text(this.team2Name);
   }
 
 }
