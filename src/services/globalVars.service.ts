5a73b080ea7102aff715a9ce42b4fa2fe8020165
andreakpresley <andreapresley5@gmail.com>
2017-08-06 22:04:54 -0500

    card buttons functionality
    -added vars for team name text
    -added global variables for keeping score
    -added scoring functions to buttons

--- src/services/globalVars.service.ts
+++ src/services/globalVars.service.ts
@@ -1,14 +1,28 @@
 import {Injectable} from '@angular/core';
 
+export enum Team {
+  team1,
+  team2
+}
+
 @Injectable()
 export class GlobalVarsService {
 
+  //validate what text they can enter for team names in settings?
+
   private team1score;
   private team2score;
+  private currentTeam;
+  private team1Text;
+  private team2Text;
+  // public teamTypes = Team;
 
   constructor() {
     this.team1score = 0;
     this.team2score = 0;
+    this.currentTeam = Team.team1;
+    this.team1Text = "Team 1";
+    this.team2Text = "Team 2";
   }
 
   public setTeam1Score(value) {
@@ -25,6 +39,38 @@ export class GlobalVarsService {
 
   public getTeam2Score() {
     return this.team2score;
+  }
+
+  public setCurrentTeam(value) {
+    this.currentTeam = value;
+  }
+
+  public getCurrentTeam() {
+    return this.currentTeam;
+  }
+
+  public setTeam1Text(value) {
+    this.team1Text = value;
+  }
+
+  public getTeam1Text() {
+    return this.team1Text;
+  }
+
+  public setTeam2Text(value) {
+    this.team2Text = value;
+  }
+
+  public getTeam2Text() {
+    return this.team2Text;
+  }
+
+  public getCurrentTeamText() {
+    if(this.getCurrentTeam() == Team.team1) {
+      return this.team1Text;
+    } else {
+      return this.team2Text;
+    }
   }
 
 }
